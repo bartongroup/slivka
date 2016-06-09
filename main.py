4ae5f0c522b89f597f671fe87283aa2f47fd32ad
mmwarowny <mmwarowny@dundee.ac.uk>
2016-06-09 13:44:22 +0100

    Worker succesfully communicates with clients.
    Jobs can be properly spawned with the worker.
    Parallel thread collects and executes jobs from the queue.
    DeferredResult correctly retrieves job status and reslt from the worker.

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 #!usr/bin/env
 
 import sys
-import time
 
-from task_queue import queue_run
-
-
-def test_worker():
-    from dummy_task import C
-    c = C()
-    res = queue_run(c)
-    print(res)
-    print(res.status)
-    print(res.result)
-    time.sleep(11)
-    print(res.status)
-    print(res.result)
 
 if "--worker" in sys.argv:
     from task_queue.worker import start_worker
     start_worker()
-else:
-    test_worker()
