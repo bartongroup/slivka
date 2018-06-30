12aeb271ac4cc926a406d84a386e6fa7c7229e15
Mateusz Warowny <warownia1@gmail.com>
2018-06-30 16:00:18 +0100

    Create openAPI3/swagger documentation.
    Improve responses provided by the server and specify REST API
    for interactions between the server and clients using OAPI3.
    Include YAML file with the schema of all the endpoints, requests
    and possible responses.
    Include swagger files into the project and serve them at /api/.
    Close #32

--- slivka/server/serverapp.py
+++ slivka/server/serverapp.py
@@ -15,6 +15,7 @@ import tempfile
 
 import flask
 import itsdangerous
+import pkg_resources
 import sqlalchemy.orm.exc
 import werkzeug.exceptions
 import werkzeug.utils
@@ -37,10 +38,19 @@ def get_services():
 
     :return: JSON response with list of service names
     """
-    return JsonResponse({"services": slivka.settings.services})
+    return JsonResponse({
+        'statuscode': 200,
+        'services': [
+            {
+                'name': service,
+                'submitURI': '/service/%s/form' % service
+            }
+            for service in slivka.settings.services
+        ]
+    })
 
 
-@app.route('/service/<service>/form', methods=["GET"])
+@app.route('/service/<service>/form', methods=['GET'])
 def get_service_form(service):
     """Gets service request form. ``GET /service/{service}/form``
 
@@ -52,18 +62,20 @@ def get_service_form(service):
     form_cls = FormFactory().get_form_class(service)
     form = form_cls()
     response = {
-        "form": form_cls.__name__,
-        "service": service,
-        "fields": [
+        'statuscode': 200,
+        'form': form_cls.__name__,
+        'service': service,
+        'submitURI': '/service/%s/form' % service,
+        'fields': [
             {
-                "name": field.name,
-                "type": field.type,
-                "required": field.required,
-                "default": field.default,
-                "constraints": [
+                'name': field.name,
+                'type': field.type,
+                'required': field.required,
+                'default': field.default,
+                'constraints': [
                     {
-                        "name": snake_to_camel(constraint['name']),
-                        "value": constraint['value']
+                        'name': snake_to_camel(constraint['name']),
+                        'value': constraint['value']
                     }
                     for constraint in field.constraints
                 ]
@@ -74,10 +86,10 @@ def get_service_form(service):
     return JsonResponse(response, status=200)
 
 
-@app.route('/service/<service>/form', methods=["POST"])
+@app.route('/service/<service>/form', methods=['POST'])
 def post_service_form(service):
     """Send form data and starts new task. ``POST /service/{service}/form``
-    
+
     :param service: service name
     :return: JSON response with submitted task id
     """
@@ -90,33 +102,33 @@ def post_service_form(service):
             job_request = form.save(session)
             session.commit()
             response = JsonResponse({
-                "taskId": job_request.uuid
+                'statuscode': 202,
+                'taskId': job_request.uuid,
+                'checkStatusURI': '/task/%s/status' % job_request.uuid
             }, status=202)
     else:
         response = JsonResponse({
-            "errors": [{
-                "field": name,
-                "errorCode": error.code,
-                "message": error.reason
+            'errors': [{
+                'statuscode': 420,
+                'field': name,
+                'errorCode': error.code,
+                'message': error.reason
             } for name, error in form.errors.items()]
         }, status=420)
     return response
 
 
-@app.route('/file', methods=["POST"])
+@app.route('/file', methods=['POST'])
 def file_upload():
     """Upload the file to the server. ``POST /file``
 
     :return: JSON containing internal metadata of the uploaded file
     """
     try:
-        mimetype = request.form["mimetype"]
+        mimetype = request.form['mimetype']
+        file = request.files['file']
     except KeyError:
-        return JsonResponse({"error": "no mimetype"}, 400)
-    try:
-        file = request.files["file"]
-    except KeyError:
-        return JsonResponse({"error": "no file"}, 400)
+        raise abort(400)
     filename = werkzeug.utils.secure_filename(file.filename)
     with tempfile.NamedTemporaryFile(
             dir=app.config['MEDIA_DIR'], delete=False) as tf:
@@ -131,20 +143,22 @@ def file_upload():
         session.commit()
         file_id = file_record.id
     return JsonResponse({
-        "id": file_id,
-        "signedId":
+        'statuscode': 201,
+        'id': file_id,
+        'signedId':
             signer.sign(itsdangerous.want_bytes(file_id)).decode('utf-8'),
-        "title": filename,
-        "mimetype": mimetype
-    }, status=203)
+        'title': filename,
+        'mimetype': mimetype,
+        'downloadURI': '/file/%s/download' % file_id
+    }, status=201)
 
 
-@app.route('/file/<file_id>', methods=["GET"])
+@app.route('/file/<file_id>', methods=['GET'])
 def get_file_meta(file_id):
     """Get file metadata. ``GET /file/{file_id}``
-    
+
     :param file_id: file identifier
-    :return: JSON containing internal metadata of the file 
+    :return: JSON containing internal metadata of the file
     """
     session = Session()
     try:
@@ -156,16 +170,18 @@ def get_file_meta(file_id):
     finally:
         session.close()
     return JsonResponse({
-        "id": file.id,
-        "title": file.title,
-        "mimetype": file.mimetype
+        'statuscode': 200,
+        'id': file.id,
+        'title': file.title,
+        'mimetype': file.mimetype,
+        'downloadURI': '/file/%s/download' % file.id
     }, status=200)
 
 
-@app.route('/file/<file_id>/download', methods=["GET"])
+@app.route('/file/<file_id>/download', methods=['GET'])
 def file_download(file_id):
     """Download file contents. ``GET /file/{file_id}/download``
-    
+
     :param file_id: file identifier
     :return: requested file contents
     """
@@ -184,18 +200,17 @@ def file_download(file_id):
         )
 
 
-@app.route('/file/<signed_file_id>', methods=["PUT"])
+@app.route('/file/<signed_file_id>', methods=['PUT'])
 def set_file_meta(signed_file_id):
     """Update file metadata. ``PUT /file/{signed_file_id}``
 
-    
     :param signed_file_id: signed file identifier
-    :return: JSON containing new metadata of the file 
+    :return: JSON containing new metadata of the file
     """
     try:
         file_id = signer.unsign(signed_file_id).decode('utf-8')
     except itsdangerous.BadSignature:
-        return JsonResponse({'error': "invalid signature"}, 403)
+        raise abort(401)
     with start_session() as session:
         try:
             file = (session.query(models.File).
@@ -203,29 +218,31 @@ def set_file_meta(signed_file_id):
                     one())
         except sqlalchemy.orm.exc.NoResultFound:
             raise abort(404)
-        new_title = request.form.get("title")
+        new_title = request.form.get('title')
         if new_title is not None:
             file.title = new_title
         session.commit()
         return JsonResponse({
-            "id": file.id,
-            "title": file.title,
-            "mimetype": file.mimetype
+            'statuscode': 200,
+            'id': file.id,
+            'signedId': signed_file_id,
+            'title': file.title,
+            'mimetype': file.mimetype,
+            'downloadURI': '/file/%s/download' % file.id
         }, status=200)
 
 
-@app.route('/file/<signed_file_id>', methods=["DELETE"])
+@app.route('/file/<signed_file_id>', methods=['DELETE'])
 def delete_file(signed_file_id):
     """Delete file from the filesystem. ``DELETE /file/{signed_file_id}``
 
-    
     :param signed_file_id: signed file identifier
     :return: Http response with status code
     """
     try:
         file_id = signer.unsign(signed_file_id).decode('utf-8')
     except itsdangerous.BadSignature:
-        return JsonResponse({'error': "invalid signature"}, 403)
+        raise abort(401)
     path = None
     with start_session() as session:
         try:
@@ -241,13 +258,13 @@ def delete_file(signed_file_id):
         os.remove(path)
     except FileNotFoundError:
         raise abort(404)
-    return Response(status=204)
+    return JsonResponse({'statuscode': 200}, status=200)
 
 
 @app.route('/task/<task_id>/status', methods=['GET'])
 def get_task_status(task_id):
     """Get the status of the task. ``GET /task/{task_id}/status``
-    
+
     :param task_id: task identifier
     :return: JSON response with current job completion status
     """
@@ -259,17 +276,19 @@ def get_task_status(task_id):
         except sqlalchemy.orm.exc.NoResultFound:
             raise abort(404)
         return JsonResponse({
-            "status": job_req.status,
-            "ready": job_req.is_finished
+            'statuscode': 200,
+            'execution': job_req.status,
+            'ready': job_req.is_finished,
+            'resultURI': '/task/%s/result' % task_id
         })
 
 
-@app.route('/task/<task_id>/files', methods=['GET'])
-def get_task_files(task_id):
-    """Get the list of file ids of this job. ``GET /task/{task_id}/files``
+@app.route('/task/<task_id>/result', methods=['GET'])
+def get_task_result(task_id):
+    """Get the list of output files. ``GET /task/{task_id}/files``
 
     :param task_id: task identifier
-    :return: JSON response with list of file ids produced by the task.
+    :return: JSON response with list of files produced by the task.
     """
     with start_session() as session:
         try:
@@ -283,8 +302,37 @@ def get_task_files(task_id):
                  filter_by(job=req.job).
                  all())
         return JsonResponse({
-            "files": [file.id for file in files]
-        })
+            'statuscode': 200,
+            'files': [
+                {
+                    'id': file.id,
+                    'title': file.title,
+                    'mimetype': file.mimetype,
+                    'downloadURI': '/file/%s/download' % file.id
+                }
+                for file in files
+            ]
+        }, status=200)
+
+
+@app.route('/api/')
+def api_index():
+    path = pkg_resources.resource_filename('slivka', 'data/swagger-ui-dist/')
+    return flask.send_from_directory(path, 'index.html')
+
+
+@app.route('/api/openapi.yaml')
+def serve_openapi_yaml():
+    stream = pkg_resources.resource_stream(
+        'slivka', 'data/openapi-docs/openapi.yaml'
+    )
+    return flask.send_file(stream, 'application/yaml', as_attachment=False)
+
+
+@app.route('/api/<path:filename>')
+def serve_api_static(filename=None):
+    path = pkg_resources.resource_filename('slivka', 'data/swagger-ui-dist/')
+    return flask.send_from_directory(path, filename)
 
 
 @app.route('/echo', methods=['GET', 'POST', 'PUT', 'DELETE'])
@@ -303,25 +351,8 @@ def echo():
     )
 
 
-# noinspection PyUnusedLocal
-@app.errorhandler(404)
-def not_found_404(e):
-    """Custom 404 error JSON response."""
-    return JsonResponse({"error": "not found"}, 404)
-
-
-# noinspection PyUnusedLocal
-@app.errorhandler(405)
-def not_allowed_405(e):
-    """Custom 405 error JSON response."""
-    return JsonResponse({"error": "method not allowed"}, 405)
-
-
-# noinspection PyUnusedLocal
-@app.errorhandler(500)
-def server_error_500(e):
-    """Custom 500 error JSON resoinse."""
-    return JsonResponse({"error": "internal server error"}, 500)
+def error_response(status, message):
+    return JsonResponse({'statuscode': status, 'error': message}, status)
 
 
 # noinspection PyPep8Naming
@@ -340,6 +371,29 @@ def JsonResponse(content, status=200, **kwargs):
     return Response(
         response=json.dumps(content, indent=2),
         status=status,
-        mimetype="application/json",
+        mimetype='application/json',
         **kwargs
     )
+
+
+app.register_error_handler(
+    400, lambda e: error_response(400, 'Bad request')
+)
+app.register_error_handler(
+    401, lambda e: error_response(401, 'Unauthorized')
+)
+app.register_error_handler(
+    404, lambda e: error_response(404, 'Not found')
+)
+app.register_error_handler(
+    405, lambda e: error_response(405, 'Method not allowed')
+)
+app.register_error_handler(
+    415, lambda e: error_response(415, 'Unsupported media type')
+)
+app.register_error_handler(
+    500, lambda e: error_response(500, 'Internal server error')
+)
+app.register_error_handler(
+    503, lambda e: error_response(503, 'Service unavailable')
+)
