b6a2aeaf74781ece3bb8e486417d5da9e1e832a7
Mateusz Warowny <warownia1@gmail.com>
2018-07-26 12:07:52 +0100

    Provide key classes from the top module.

--- slivka/__init__.py
+++ slivka/__init__.py
@@ -1,3 +1,5 @@
 from slivka.settings import SettingsProvider
+from slivka.scheduler.limits import LimitsBase
+from slivka.scheduler.execution_manager import JobHandler, JobStatus, Runner
 
 settings = SettingsProvider()
