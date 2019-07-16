6bdd6507b6c0f3ee1fe56f0ce32441a57119aa80
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2019-07-16 12:40:36 +0100

    Use default file validator if not provided.
    Whenever the accepted file type has no associated validator, a default pass-everything validator will be used and a warning will be issued.
    Similarly, if an unknown mime-type is given, a reject-everything validator is used instead.

--- slivka/server/file_validators.py
+++ slivka/server/file_validators.py
@@ -1,4 +1,5 @@
 import io
+from warnings import warn
 
 import slivka.utils
 
@@ -16,6 +17,8 @@ def biopython_validator_factory(file_format):
         except (ValueError, IndexError):
             return False
         finally:
+            # detach the buffer or it will close the underlying stream
+            # when leaving the function scope
             buffer.detach()
     return validator
 
@@ -27,33 +30,44 @@ def plain_text_validator(file):
     return set(file.read(256)).issubset(text_chars)
 
 
-_default_validators = {
+def reject_validator(_):
+    return False
+
+
+def pass_validator(_):
+    return True
+
+
+_builtin_validators = {
     'text/plain': plain_text_validator
 }
 
 if Bio is not None:
-    _default_validators.update({
+    _builtin_validators.update({
         'application/fasta': biopython_validator_factory('fasta'),
         'application/clustal': biopython_validator_factory('clustal'),
         'application/genbank': biopython_validator_factory('genbank'),
         'application/embl': biopython_validator_factory('embl')
     })
 
-for mime_type, validator \
-        in getattr(slivka.settings, 'FILE_VALIDATORS', {}).items():
-    _default_validators[mime_type] = slivka.utils.locate(validator)
+for mime_type, path in getattr(slivka.settings, 'FILE_VALIDATORS', {}).items():
+    _builtin_validators[mime_type] = slivka.utils.locate(path)
 
 _validators = {}
 
 for file_type in slivka.settings.ACCEPTED_FILE_TYPES:
-    _validators[file_type] = _default_validators[file_type]
+    if file_type not in _builtin_validators:
+        msg = (
+            "{} has no validator set up. "
+            "Every file with this mime type will be accepted."
+            .format(file_type)
+        )
+        warn(msg, RuntimeWarning)
+    _validators[file_type] = _builtin_validators.get(file_type, pass_validator)
 
-del _default_validators
+del _builtin_validators
 
 
 def validate_file_type(file, mime_type):
-    validator = _validators.get(mime_type)
-    if validator is None:
-        return False
-    else:
-        return validator(file)
+    validator = _validators.get(mime_type, reject_validator)
+    return validator(file)
