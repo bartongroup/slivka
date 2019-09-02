c62bc1e95ef325018a628473f1d88c3070f59a98
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2019-09-02 11:15:32 +0100

    Base and Integer Field unit tests

---
+++ tests/forms/test_base_field.py
@@ -0,0 +1,49 @@
+import pytest
+
+from slivka.server.forms.fields import BaseField
+
+
+@pytest.fixture()
+def example_field():
+    return BaseField('myfield')
+
+
+def test_field_name():
+    field = BaseField('foobar')
+    assert field.name == 'foobar'
+
+
+def test_field_description():
+    field = BaseField('name', description='example description')
+    assert field.description == 'example description'
+
+
+def test_get_value_from_data(example_field):
+    data = {
+        'alpha': 1,
+        'beta': -0.53,
+        'myfield': 'myvalue',
+        'gamma': 'foobar'
+    }
+    assert example_field.value_from_request_data(data, {}) == 'myvalue'
+
+
+def test_get_missing_value_from_data(example_field):
+    data = {
+        'alpha': 1,
+        'beta': -0.53,
+        'gamma': 'foobar'
+    }
+    assert example_field.value_from_request_data(data, {}) is None
+
+
+def test_int_to_cmd_parameter(example_field: BaseField):
+    assert example_field.to_cmd_parameter(1) == '1'
+
+
+def test_str_to_cmd_parameter(example_field: BaseField):
+    assert example_field.to_cmd_parameter('foo') == 'foo'
+
+
+def test_none_to_cmd_parameter(example_field: BaseField):
+    assert example_field.to_cmd_parameter(None) is None
---
+++ tests/forms/test_integer_field.py
@@ -0,0 +1,130 @@
+import pytest
+
+from slivka.server.forms.fields import IntegerField, ValidationError
+
+
+@pytest.fixture('module')
+def default_field():
+    return IntegerField('name')
+
+
+# to_python tests
+
+def test_int_to_python(default_field):
+    assert default_field.to_python(10) == 10
+    assert default_field.to_python(-8) == -8
+
+
+def test_float_to_python(default_field):
+    with pytest.raises(ValidationError):
+        default_field.to_python(2.43)
+
+
+def test_number_str_to_python(default_field):
+    assert default_field.to_python('15') == 15
+    assert default_field.to_python('-3') == -3
+
+
+def test_decimal_str_to_python(default_field):
+    with pytest.raises(ValidationError):
+        default_field.to_python('0.65')
+
+
+def test_other_str_to_python(default_field):
+    with pytest.raises(ValidationError):
+        default_field.to_python('xyz')
+
+
+def test_none_to_python(default_field):
+    assert default_field.to_python(None) is None
+    assert default_field.to_python('') is None
+
+
+def test_bool_to_python(default_field):
+    with pytest.raises(ValidationError):
+        default_field.to_python(True)
+    with pytest.raises(ValidationError):
+        default_field.to_python(False)
+
+
+def test_zero_to_python(default_field):
+    assert default_field.to_python(0) is 0
+
+
+# min/max validation
+
+def test_validate_more_than_max():
+    field = IntegerField('name', max=7)
+    with pytest.raises(ValidationError):
+        field.validate(8)
+
+
+def test_validate_equal_to_max():
+    field = IntegerField('name', max=7)
+    assert field.validate(7) == 7
+
+
+def test_validate_less_than_max():
+    field = IntegerField('name', max=7)
+    assert field.validate(6) == 6
+
+
+def test_validate_more_than_min():
+    field = IntegerField('name', min=7)
+    assert field.validate(8) == 8
+
+
+def test_validate_equal_to_min():
+    field = IntegerField('name', min=7)
+    assert field.validate(7)
+
+
+def test_validate_less_than_min():
+    field = IntegerField('name', min=7)
+    with pytest.raises(ValidationError):
+        field.validate(6)
+
+
+# empty value validation
+
+def test_validate_none(default_field):
+    with pytest.raises(ValidationError):
+        default_field.validate(None)
+
+
+def test_validate_none_required():
+    field = IntegerField('name', required=True)
+    with pytest.raises(ValidationError):
+        field.validate(None)
+
+
+def test_validate_none_not_required():
+    field = IntegerField('name', required=False)
+    assert field.validate(None) is None
+
+
+# validation with default
+
+def test_validate_none_with_default():
+    field = IntegerField('name', default=5)
+    assert field.validate(None) == 5
+    field = IntegerField('name', default=0)
+    assert field.validate(None) == 0
+
+
+def test_validate_empty_with_default():
+    field = IntegerField('name,', default=3)
+    assert field.validate('') == 3
+    assert field.validate(()) == 3
+
+
+def validate_valid_value_with_default():
+    field = IntegerField('name', default=4, max=8)
+    assert field.validate(1) == 1
+    assert field.validate(0) == 0
+
+
+def validate_invalid_value_with_default():
+    field = IntegerField('name', default=4, max=8)
+    with pytest.raises(ValidationError):
+        field.validate(10)
