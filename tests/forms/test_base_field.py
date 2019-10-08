9c36dbbe5bab3d4cefe61ea78b0296bd635b660f
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2019-10-08 17:53:31 +0100

    Tests: value validation of BaseField

--- tests/forms/test_base_field.py
+++ tests/forms/test_base_field.py
@@ -1,6 +1,21 @@
 import pytest
+from werkzeug.datastructures import MultiDict
 
-from slivka.server.forms.fields import BaseField
+from slivka.server.forms.fields import BaseField, ValidationError
+
+
+class BaseFieldStub(BaseField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        def validator(val):
+            if val == 'FAIL':
+                raise ValidationError("MESSAGE", 'code')
+
+        self.validators.append(validator)
+
+    def to_python(self, value):
+        return value
 
 
 @pytest.fixture()
@@ -37,6 +52,35 @@ def test_get_missing_value_from_data(example_field):
     assert example_field.value_from_request_data(data, {}) is None
 
 
+def test_validate_valid_value():
+    field = BaseFieldStub('name')
+    assert field.validate('value') == 'value'
+    assert field.validate('') == ''
+
+
+def test_validate_invalid_value():
+    field = BaseFieldStub('name')
+    with pytest.raises(ValidationError):
+        field.validate('FAIL')
+
+
+def test_validate_missing_required_value():
+    field = BaseFieldStub('name')
+    with pytest.raises(ValidationError):
+        field.validate(None)
+
+
+def test_validate_missing_not_required_value():
+    field = BaseFieldStub('name', required=False)
+    assert field.validate(None) is None
+
+
+def test_validate_with_default_value():
+    field = BaseFieldStub('name', default='value')
+    assert field.validate(None) == 'value'
+    assert field.validate('other value') == 'other value'
+
+
 def test_int_to_cmd_parameter(example_field: BaseField):
     assert example_field.to_cmd_parameter(1) == '1'
 
@@ -55,3 +99,37 @@ def test_true_to_cmd_parameter(example_field: BaseField):
 
 def test_false_to_cmd_parameter(example_field: BaseField):
     assert bool(example_field.to_cmd_parameter(False)) is False
+
+
+def test_get_multiple_value_from_data():
+    field = BaseField('name', multiple=True)
+    data = MultiDict([('name', 'a'), ('name', 'b')])
+    assert field.value_from_request_data(data, MultiDict()) == ['a', 'b']
+
+
+def test_multiple_missing_value_from_data():
+    field = BaseField('name', multiple=True)
+    assert field.value_from_request_data(MultiDict(), MultiDict()) == []
+
+
+def test_validate_multiple_valid_values():
+    field = BaseFieldStub('name', multiple=True)
+    assert field.validate(['foo', 'bar']) == ['foo', 'bar']
+
+
+def test_validate_multiple_no_values():
+    field = BaseFieldStub('name', multiple=True)
+    with pytest.raises(ValidationError):
+        field.validate([])
+
+
+def test_validate_multiple_with_default():
+    field = BaseFieldStub('name', multiple=True, default='default')
+    assert field.validate(None) == ['default']
+    assert field.validate([]) == ['default']
+
+
+def test_validate_multiple_containing_invalid():
+    field = BaseFieldStub('name', multiple=True)
+    with pytest.raises(ValidationError):
+        field.validate(['x', 'y', 'FAIL', 'z'])
