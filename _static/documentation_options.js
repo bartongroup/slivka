6e38e8fc84831cafc55dd7899bef8f356cd63673
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2019-10-14 17:16:05 +0100

    Documentation v0.5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.a5',
+    VERSION: '0.5.b3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
