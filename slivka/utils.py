a1c601a39f02c383a225b22c1ea021ea57f579c1
MMWarowny <mmzwarowny@dundee.ac.uk>
2019-10-25 12:15:36 +0100

    Remove redundant functions in utils module.

--- slivka/utils.py
+++ slivka/utils.py
@@ -1,7 +1,5 @@
 import enum
 import os
-import re
-import shutil
 from collections import OrderedDict
 
 import yaml.resolver
@@ -78,100 +76,6 @@ SafeTranscludingOrderedYamlLoader.add_constructor(
 SafeTranscludingOrderedYamlLoader.add_constructor(
     '!include', _include_constructor
 )
-
-
-# ## Path and file traversal utilities ##
-
-def locate(path):
-    """Imports any module or object accessible from the PYTHONPATH.
-
-    Improved implementation of the __import__ function that can fetch
-    members and attributes in addition to the modules.
-
-    :param path: dot separated path to the object
-    :return: object
-    :raises ModuleNotFoundError: if the module is not found
-    :raises AttributeError: if the object attribute is missing
-    """
-    parts = path.split('.')
-    n = 0
-    obj = None
-    while n < len(parts):
-        import_path = '.'.join(parts[:len(parts)-n])
-        try:
-            obj = __import__(import_path)
-        except ImportError as e:
-            if e.name != import_path:
-                raise
-            n += 1
-        else:
-            break
-    if obj is None:
-        raise ImportError('No module named \'%s\'' % parts[0])
-    for part in parts[1:]:
-        obj = getattr(obj, part)
-    return obj
-
-
-def copytree(src, dst):
-    """Copy directory tree recursively.
-    
-    Alternative implementation of shutil.copytree which allows to copy into
-    existing directories. Directories which does not exist are created and
-    existing directories are populated with files and folders.
-    If destination directory path does not exist, it will attempt to create
-    the entire directory tree up to that level.
-    
-    :param src: source directory path
-    :param dst: destination directory path
-    :return: destination directory path
-    """
-    os.makedirs(dst, exist_ok=True)
-    errors = []
-    for name in os.listdir(src):
-        srcname = os.path.join(src, name)
-        dstname = os.path.join(dst, name)
-        try:
-            if os.path.isdir(srcname):
-                copytree(srcname, dstname)
-            else:
-                shutil.copy(srcname, dstname)
-        except shutil.Error as err:
-            errors.extend(err.args[0])
-        except OSError as err:
-            errors.append((srcname, dstname, str(err)))
-    if errors:
-        raise shutil.Error(errors)
-    return dst
-
-
-def recursive_scandir(path='.'):
-    """Returns an iterator of ``os.DirEntry`` of files in the directory."""
-    for entry in os.scandir(path):
-        if entry.is_dir():
-            yield from recursive_scandir(entry.path)
-        else:
-            yield entry
-
-
-def snake_to_camel(name):
-    """Convert snake_case name to lowercase camelCase
-    
-    :param name: snake_case name
-    :return: camelCase name
-    """
-    comp = name.split('_')
-    return comp[0] + ''.join(map(str.capitalize, comp[1:]))
-
-
-def camel_to_snake(name):
-    """Convert camelCase name to snake_case
-    
-    :param name: camelCase name
-    :return: snake_case name
-    """
-    s = re.sub('([A-Z])([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s).lower()
 
 
 class JobStatus(enum.IntEnum):
