095ba82fcd8f32b4a2498cfe1abf29fd205317db
MMWarowny <mmzwarowny@dundee.ac.uk>
2019-11-07 16:24:34 +0000

    Add TIMEOUT and REQ_RELAXED options to the local queue client socket.
    Fix #70

--- slivka/local_queue/client.py
+++ slivka/local_queue/client.py
@@ -14,13 +14,18 @@ class LocalQueueClient:
         self.address = protocol + '://' + address
         self.secret = secret
         self.socket = zmq_ctx.socket(zmq.REQ)
+        self.socket.setsockopt(zmq.RCVTIMEO, 100)
+        self.socket.setsockopt(zmq.REQ_RELAXED, 1)
         self.socket.connect(self.address)
 
     def submit_job(self, cmd, cwd, env):
         self.socket.send_json({
             'method': 'POST', 'cmd': cmd, 'cwd': cwd, 'env': env
         })
-        response = self.socket.recv_json()
+        try:
+            response = self.socket.recv_json()
+        except zmq.error.Again as e:
+            raise ConnectionError("Could not connect to %s." % self.address) from None
         if response.pop('ok'):
             return self.JobStatusResponse(**response)
         else:
