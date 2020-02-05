27bf6752c31e2a6cf2778eb8ad7b9a6e48e455cb
MMWarowny <mmzwarowny@dundee.ac.uk>
2020-02-05 12:03:14 +0000

    FileField json representation fix

--- slivka/server/forms/fields.py
+++ slivka/server/forms/fields.py
@@ -382,7 +382,7 @@ class FileField(BaseField):
         j = super().__json__()
         j['type'] = 'file'
         if self.media_type is not None: j['mimetype'] = self.media_type
-        if self.media_type is not None: j['extensions'] = self.extensions
+        if self.extensions: j['extensions'] = self.extensions
         return j
 
     def to_cmd_parameter(self, value: 'FileWrapper'):
