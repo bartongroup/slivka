ef80eb95801e3dc777cf056e3fe39132ff04d64e
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-04-09 12:48:11 +0100

    Select loader based on settings file version.

--- slivka/conf/__init__.py
+++ slivka/conf/__init__.py
@@ -1,3 +1,7 @@
+import os
+
+import yaml
+
 from slivka.utils import cached_property
 from . import loaders
 
@@ -6,7 +10,20 @@ class SettingsProxy:
     settings_file = None
 
     def __init__(self):
-        self.loader = loaders.SettingsLoaderV10()
+        if 'SLIVKA_SETTINGS' in os.environ:
+            settings_path = os.environ['SLIVKA_SETTINGS']
+        else:
+            home = os.getenv('SLIVKA_HOME', os.getcwd())
+            settings_path = os.path.join(home, 'settings.yaml')
+            if not os.path.isfile(settings_path):
+                settings_path = os.path.join(home, 'settings.yml')
+        version = yaml.safe_load(open(settings_path)).get('VERSION', '1.0')
+        if version == '1.0':
+            self.loader = loaders.SettingsLoaderV10()
+        elif version == '1.1':
+            self.loader = loaders.SettingsLoaderV11()
+        else:
+            raise ValueError("Invalid settings version %s" % version)
 
     @cached_property
     def settings(self) -> loaders.Settings:
