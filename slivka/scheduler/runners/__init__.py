33b3313a6f5ccbfaa38b7502559a4d0cc9b620b0
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2021-06-07 14:05:53 +0200

    Include Command and Job classes from runner module in runners package.

--- slivka/scheduler/runners/__init__.py
+++ slivka/scheduler/runners/__init__.py
@@ -1,8 +1,9 @@
 from .grid_engine import GridEngineRunner
-from .runner import Runner
+from .runner import Runner, Command, Job
 from .shell import ShellRunner
 from .slivka_queue import SlivkaQueueRunner
 
 __all__ = (
-    'Runner', 'GridEngineRunner', 'ShellRunner', 'SlivkaQueueRunner'
+    'Runner', 'GridEngineRunner', 'ShellRunner', 'SlivkaQueueRunner',
+    'Command', 'Job'
 )
