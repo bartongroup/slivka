5a293c981432f8ec79fb71487eca1663563d3200
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2021-06-07 18:39:26 +0200

    Updated runner tests, env and args tests gathered in a single test file.

---
+++ tests/runner/test_base_runner.py
@@ -0,0 +1,177 @@
+import os
+
+from nose.tools import (
+    assert_equal, assert_sequence_equal, assert_in,
+    assert_list_equal
+)
+
+from slivka.conf import ServiceConfig
+from .stub import RunnerStub
+
+Argument = ServiceConfig.Argument
+
+
+def setup_module():
+    os.environ['SLIVKA_HOME'] = '/tmp/slivkahome'
+    os.environ['GLOBAL'] = 'global'
+
+
+def teardown_module():
+    del os.environ['SLIVKA_HOME']
+
+
+class EnvTest:
+    def setup(self):
+        self.runner = RunnerStub(
+            None,
+            command="/bin/${MY_VAR}",
+            args=[
+                Argument('param', "--${MY_VAR}"),
+                Argument('value', "$(value)")
+            ],
+            outputs=[],
+            env={
+                "EXAMPLE": "example",
+                "BIN_PATH": "${SLIVKA_HOME}/bin",
+                "MY_VAR": "foobar"
+            }
+        )
+
+    def test_vars_present(self):
+        for var in ['PATH', 'SLIVKA_HOME', 'EXAMPLE', 'BIN_PATH', 'MY_VAR']:
+            yield self._check_in_env, var
+
+    def _check_in_env(self, item):
+        assert_in(item, self.runner.env)
+
+    def test_slivka_home(self):
+        assert_equal(self.runner.env['SLIVKA_HOME'], '/tmp/slivkahome')
+
+    def test_env_interpolation(self):
+        assert_equal(self.runner.env['BIN_PATH'], '/tmp/slivkahome/bin')
+
+    def test_arg_interpolation(self):
+        assert_sequence_equal(
+            self.runner.build_args({'param': '1'}), ['--foobar']
+        )
+
+    @staticmethod
+    def test_command_interpolation_global():
+        runner = RunnerStub(command="/bin/${GLOBAL}")
+        assert_sequence_equal(runner.command, ["/bin/global"])
+
+    def test_command_interpolation_defined(self):
+        assert_sequence_equal(self.runner.command, ["/bin/foobar"])
+
+    def test_env_var_injection(self):
+        assert_sequence_equal(
+            self.runner.build_args({'value': '$EXAMPLE'}), ["$EXAMPLE"]
+        )
+        assert_sequence_equal(
+            self.runner.build_args({'value': "${EXAMPLE}"}), ["${EXAMPLE}"]
+        )
+
+
+class TestBuildArgs:
+    @staticmethod
+    def _check_args(runner, inputs, args):
+        assert_list_equal(runner.build_args(inputs), args)
+
+    def test_symbol_delimited_option(self):
+        runner = RunnerStub(
+            args=[Argument('myoption', '--option=$(value)')],
+        )
+        yield self._check_args, runner, {}, []
+        yield self._check_args, runner, {'myoption': None}, []
+        yield self._check_args, runner, {'myoption': ''}, ['--option=']
+        yield self._check_args, runner, {'myoption': 'my value'}, ["--option=my value"]
+
+    def test_space_delimited_option(self):
+        runner = RunnerStub(
+            args=[Argument('myoption', '--option $(value)')],
+        )
+        yield (self._check_args, runner,
+               {'myoption': 'value'}, ['--option', 'value'])
+        yield (self._check_args, runner,
+               {'myoption': 'my value'}, ['--option', 'my value'])
+        yield (self._check_args, runner,
+               {'myoption': 'my \'fun \' value'},
+               ['--option', 'my \'fun \' value'])
+
+    def test_quoted_option(self):
+        runner = RunnerStub(
+            args=[Argument('myoption', "'--option $(value)'"),
+                  Argument('otheropt', "\"--option $(value)\"")],
+        )
+        yield self._check_args, runner, {'myoption': 'value'}, ['--option value']
+        yield self._check_args, runner, {'myoption': 'my value'}, ['--option my value']
+        yield self._check_args, runner, {'otheropt': 'my value'}, ["--option my value"]
+        yield (self._check_args, runner,
+               {'myoption': 'my \'fun \' value'},
+               ['--option my \'fun \' value'])
+        yield (self._check_args, runner,
+               {'otheropt': 'my \'fun \' value'},
+               ['--option my \'fun \' value'])
+
+    def test_flag_option(self):
+        runner = RunnerStub(
+            args=[Argument('myflag', "--flag")]
+        )
+        yield self._check_args, runner, {}, []
+        yield self._check_args, runner, {'myflag': False}, []
+        yield self._check_args, runner, {'myflag': None}, []
+        yield self._check_args, runner, {'myflag': "true"}, ["--flag"]
+
+    def test_default_substitution(self):
+        runner = RunnerStub(
+            args=[Argument('input', "-v=$(value)", default="default")]
+        )
+        yield self._check_args, runner, {'input': 'foo'}, ['-v=foo']
+        yield self._check_args, runner, {'input': None}, ['-v=default']
+        yield self._check_args, runner, {'input': ''}, ['-v=']
+        yield self._check_args, runner, {}, ["-v=default"]
+
+    def test_parameters_ordering(self):
+        runner = RunnerStub(
+            args=[
+                Argument('iters', "--iter=$(value)"),
+                Argument('outfile', "-o $(value)"),
+                Argument('verbose', "-V"),
+                Argument('infile', "$(value)")
+            ]
+        )
+        yield self._check_args, runner, {}, []
+        yield (
+            self._check_args, runner,
+            {'iters': "5", 'outfile': 'output.txt', 'verbose': "true", 'infile': 'input.txt'},
+            ['--iter=5', '-o', 'output.txt', '-V', 'input.txt']
+        )
+        yield (
+            self._check_args, runner,
+            {'iters': "3", 'verbose': False, 'infile': 'input.txt'},
+            ['--iter=3', 'input.txt']
+        )
+        yield (
+            self._check_args, runner,
+            {'outfile': 'out.out', 'verbose': "true", 'iters': "10"},
+            ['--iter=10', '-o', 'out.out', '-V']
+        )
+
+    def test_file_input(self):
+        runner = RunnerStub(
+            args=[Argument('input', "$(value)", symlink="input.in")]
+        )
+        yield self._check_args, runner, {'input': "my_file.txt"}, ["input.in"]
+        yield self._check_args, runner, {'input': None}, []
+        yield self._check_args, runner, {}, []
+
+    def test_repeated_array_input(self):
+        runner = RunnerStub(args=[Argument('array', "-m=$(value)")])
+        yield (self._check_args, runner,
+               {'array': ['a', 'b', 'c', 'd']},
+               ['-m=a', '-m=b', '-m=c', '-m=d'])
+        yield self._check_args, runner, {'array': []}, []
+
+    def test_joined_array_input(self):
+        runner = RunnerStub(args=[Argument('array', "-m=$(value)", join=",")])
+        yield self._check_args, runner, {'array': ["a", "b", "c"]}, ["-m=a,b,c"]
--- tests/runner/test_run.py
+++ tests/runner/test_run.py
@@ -1,158 +1,138 @@
-import contextlib
 import filecmp
 import os
 import tempfile
 import unittest.mock as mock
-from collections import OrderedDict
 from itertools import zip_longest
 
 from nose.tools import assert_equal, assert_list_equal
 
+import slivka.conf
 from slivka import JobStatus
-from .stubs import runner_factory, RunnerStub
+from slivka.scheduler.runners import Job
+from .stub import RunnerStub
+
+Argument = slivka.conf.ServiceConfig.Argument
 
 
 # single job run tests
 
 def test_run_count():
-    runner = runner_factory(base_command=['mycommand'])
+    runner = RunnerStub(command=['mycommand'])
     runner.submit = mock.Mock(return_value='0xc0ffee')
-    runner.start({})
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.start({}, cwd)
     assert_equal(runner.submit.call_count, 1)
 
 
 def test_run_base_command_with_no_parameters():
-    runner = runner_factory(base_command=['mycommand'])
-    submit_mock = mock.Mock(return_value='0xc0ffee')
-    runner.submit = submit_mock
-    runner.start({})
-    args, kwargs = submit_mock.call_args
-    cmd, _ = args
-    assert_list_equal(cmd, ['mycommand'])
-
-
-def test_run_command_with_arguments():
-    runner = runner_factory(base_command='mycommand', arguments=['foo', 'bar', 'baz'])
-    runner.submit = mock.Mock(return_value='0xc0ffee')
-    runner.start({})
+    runner = RunnerStub(command=['mycommand'])
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.submit = mock.Mock(return_value=Job('0xc0ffee', cwd))
+        runner.start({}, cwd)
     args, kwargs = runner.submit.call_args
-    cmd, _ = args
-    assert_list_equal(cmd, ['mycommand', 'foo', 'bar', 'baz'])
+    cmd, = args
+    assert_list_equal(cmd.args, ['mycommand'])
 
 
 def test_run_command_with_parameters():
-    runner = runner_factory(
-        base_command='mycommand',
-        arguments=['foo', 'bar'],
-        inputs=OrderedDict([
-            ('param1', {'arg': '-p1 $(value)'}),
-            ('param2', {'arg': '-p2 $(value)'})
-        ])
+    runner = RunnerStub(
+        command='mycommand',
+        args=[
+            Argument('param1', "-p1 $(value)"),
+            Argument('param2', "-p2 $(value)"),
+            Argument("const1", "$(value)", default="foo"),
+            Argument('const2', "$(value)", default="bar")
+        ]
     )
-    runner.submit = mock.Mock(return_value='0xc0ffee')
-    runner.start({'param1': 'xxx', 'param2': 'yyy'})
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.submit = mock.Mock(return_value=Job('0xc0ffee', cwd))
+        runner.start({'param1': 'xxx', 'param2': 'yyy'}, cwd)
     args, kwargs = runner.submit.call_args
-    cmd, _ = args
-    assert_list_equal(cmd, ['mycommand', '-p1', 'xxx', '-p2', 'yyy', 'foo', 'bar'])
+    cmd, = args
+    assert_list_equal(cmd.args, ['mycommand', '-p1', 'xxx', '-p2', 'yyy', 'foo', 'bar'])
 
 
 def test_returned_job_id():
-    runner = runner_factory()
-    runner.submit = mock.Mock(return_value='0xc0ffee')
-    job_id, job_cwd = runner.start({})
+    runner = RunnerStub()
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.submit = mock.Mock(return_value=Job('0xc0ffee', cwd))
+        job_id, job_cwd = runner.start({}, cwd)
     assert_equal(job_id, '0xc0ffee')
 
 
 def test_job_working_directory():
-    runner = runner_factory()
-    runner.submit = mock.Mock(return_value='0xc0ffee')
-    job_id, job_cwd = runner.start({})
-    assert_equal(os.path.dirname(job_cwd), RunnerStub.JOBS_DIR)
-    assert os.path.isdir(job_cwd)
-
-
-def test_working_directory_cleanup():
-    runner = runner_factory()
-    MyError = type('MyError', (Exception,), {})
-    runner.submit = mock.Mock(side_effect=MyError)
-    with contextlib.suppress(MyError):
-        runner.start({})
-    (cmd, cwd), kwargs = runner.submit.call_args
-    assert not os.path.exists(cwd)
-
-
-def test_batch_working_directory_cleanup():
-    runner = runner_factory()
-    MyError = type('MyError', (Exception,), {})
-    runner.submit = mock.Mock(side_effect=MyError)
-    with contextlib.suppress(MyError):
-        runner.batch_start([{}, {}, {}, {}])
-    for args, kwargs in runner.submit.call_args_list:
-        cmd, cwd = args
-        assert not os.path.exists(cwd)
+    runner = RunnerStub()
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.submit = mock.Mock(return_value=Job('0xc0ffee', cwd))
+        job_cwd = runner.start({}, cwd).cwd
+        assert_equal(job_cwd, cwd)
+        assert os.path.isdir(job_cwd)
 
 
 # file linking tests
 
 def test_link_created():
-    runner = runner_factory(inputs={
-        'input': {'arg': '$(value)', 'type': 'file', 'symlink': 'input.txt'}
-    })
+    runner = RunnerStub(
+        args=[Argument('input', "$(value)", symlink="input.txt")]
+    )
     infile = tempfile.NamedTemporaryFile()
     infile.write(b'hello world\n')
     infile.flush()
-    runner.submit = mock.Mock(return_value='')
-    job_id, job_cwd = runner.start({'input': infile.name})
-    path = os.path.join(job_cwd, 'input.txt')
-    assert filecmp.cmp(infile.name, path), \
-        'Files %s and %s are not identical' % (infile.name, path)
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.submit = mock.Mock(return_value=Job('', cwd))
+        runner.start({'input': infile.name}, cwd)
+        path = os.path.join(cwd, 'input.txt')
+        assert filecmp.cmp(infile.name, path), \
+            'Files %s and %s are not identical' % (infile.name, path)
 
 
 def test_batch_run_file_linking():
-    runner = runner_factory(inputs={
-        'input': {'arg': '$(value)', 'type': 'file', 'symlink': 'input.txt'}
-    })
+    runner = RunnerStub(
+        args=[Argument('input', "$(value)", symlink="input.txt")]
+    )
     infile = tempfile.NamedTemporaryFile()
     infile.write(b'hello world\n')
     infile.flush()
-    runner.submit = mock.Mock(return_value='')
-    results = runner.batch_start([{'input': infile.name}] * 5)
-    for job_id, job_cwd in results:
-        path = os.path.join(job_cwd, 'input.txt')
-        assert filecmp.cmp(infile.name, path), \
-            'Files %s and %s are not identical' % (infile.name, path)
+    with tempfile.TemporaryDirectory() as jobs_dir:
+        cwds = [os.path.join(jobs_dir, str(i)) for i in range(5)]
+        runner.submit = mock.Mock(side_effect=[Job('', cwd) for cwd in cwds])
+        runner.batch_start([{'input': infile.name}] * 5, cwds)
+        for cwd in cwds:
+            path = os.path.join(cwd, 'input.txt')
+            assert filecmp.cmp(infile.name, path), \
+                'Files %s and %s are not identical' % (infile.name, path)
 
 
 # batch run tests
 
 def test_batch_run_count():
-    runner = runner_factory(base_command=['mycommand'])
+    runner = RunnerStub(command=['mycommand'])
     runner.submit = mock.Mock(return_value='0xc0ffee')
-    runner.batch_start([{}, {}, {}])
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.batch_start([{}, {}, {}], [cwd, cwd, cwd])
     assert_equal(runner.submit.call_count, 3)
 
 
 def test_batch_run_with_parameters():
-    runner = runner_factory(
-        base_command=['mycommand'],
-        inputs={
-            'param': {'arg': '$(value)'}
-        }
+    runner = RunnerStub(
+        command=['mycommand'],
+        args=[Argument('param', "$(value)")]
     )
-    runner.submit = mock.Mock(return_value='')
     params = ['foo', 'bar', 'baz']
-    runner.batch_start([{'param': arg} for arg in params])
-    for param, (args, kwargs) in zip_longest(params, runner.submit.call_args_list):
-        cmd, cwd = args
-        assert_list_equal(cmd, ['mycommand', param])
+    with tempfile.TemporaryDirectory() as cwd:
+        runner.submit = mock.Mock(return_value=Job('', cwd))
+        runner.batch_start([{'param': arg} for arg in params], [cwd] * len(params))
+    assert_equal(runner.submit.call_count, len(params))
+    for param, call_args in zip_longest(params, runner.submit.call_args_list):
+        args, kwargs = call_args
+        assert_list_equal(args[0].args, ['mycommand', param])
 
 
 # batch status check tests
 
 def test_batch_check_status():
-    LocalRunnerStub = type('Runner', (RunnerStub,), {})
-    runner = runner_factory(cls=LocalRunnerStub)
-    LocalRunnerStub.check_status = mock.Mock(
+    runner = RunnerStub()
+    runner.check_status = mock.Mock(
         side_effect=(JobStatus.COMPLETED, JobStatus.DELETED, JobStatus.QUEUED)
     )
     stats = runner.batch_check_status([mock.Mock()] * 3)
