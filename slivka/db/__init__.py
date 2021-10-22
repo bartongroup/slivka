92b1fb7b9ebc084e6d8c8c0bdd0a43c767717a02
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2021-10-22 14:46:28 +0200

    Change server selection timeout to 2s from 2ms

--- slivka/db/__init__.py
+++ slivka/db/__init__.py
@@ -37,7 +37,7 @@ class _DBModule(types.ModuleType):
             host=cfg.host, socket=cfg.socket,
             username=cfg.username, password=cfg.password
         )
-        return pymongo.MongoClient(mongo_uri, serverSelectionTimeoutMS=2)
+        return pymongo.MongoClient(mongo_uri, serverSelectionTimeoutMS=2000)
 
     @cached_property
     def database(self):
