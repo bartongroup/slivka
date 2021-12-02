732a9ce74958f4b1e23b5e47e7ad7f45d504163d
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2021-12-02 19:54:59 +0100

    Make SlurmRunner importable from slivka.scheduler.runners package

--- slivka/scheduler/runners/__init__.py
+++ slivka/scheduler/runners/__init__.py
@@ -2,8 +2,9 @@ from .grid_engine import GridEngineRunner
 from .runner import Runner, Command, Job, RunnerID
 from .shell import ShellRunner
 from .slivka_queue import SlivkaQueueRunner
+from .slurm import SlurmRunner
 
 __all__ = (
     'Runner', 'GridEngineRunner', 'ShellRunner', 'SlivkaQueueRunner',
-    'RunnerID', 'Command', 'Job'
+    'SlurmRunner', 'RunnerID', 'Command', 'Job'
 )
