3b1b9d8f1de6127a71223aa9f1d3aca450240e10
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2023-05-17 16:10:55 +0200

    Update 'send_from_directory' usages.

--- slivka/server/media_views.py
+++ slivka/server/media_views.py
@@ -6,12 +6,12 @@ import flask
 def serve_uploads_view(file_path):
     return flask.send_from_directory(
         directory=flask.current_app.config['uploads_dir'],
-        filename=file_path
+        path=file_path
     )
 
 
 def serve_results_view(job_id, file_path):
     return flask.send_from_directory(
         directory=flask.current_app.config['jobs_dir'],
-        filename=os.path.join(job_id, file_path)
+        path=os.path.join(job_id, file_path)
     )
