2c9c3bcc8df83bf8dea1086f7f33b74eab0e2a66
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2023-11-16 14:27:49 +0100

    Fix resource loaders for python <3.11

--- slivka/compat/resources.py
+++ slivka/compat/resources.py
@@ -1,4 +1,5 @@
 import importlib.resources
+import os.path
 import sys
 from importlib.resources import Package, Resource
 from typing import BinaryIO, TextIO
@@ -11,10 +12,43 @@ __all__ = [
 ]
 
 if sys.version_info < (3, 11):
-    open_binary = importlib.resources.open_binary
-    open_text = importlib.resources.open_text
-    read_binary = importlib.resources.read_binary
-    read_text = importlib.resources.read_text
+    def _shift_path(package, resource):
+        parts = os.path.normpath(resource).split(os.path.sep)
+        return '.'.join([package, *parts[:-1]]), parts[-1]
+
+
+    def open_binary(package: Package, resource: Resource) -> BinaryIO:
+        return importlib.resources.open_binary(*_shift_path(package, resource))
+
+
+    def open_text(
+            package: Package,
+            resource: Resource,
+            encoding: str = 'utf-8',
+            errors: str = 'strict'
+    ) -> TextIO:
+        return importlib.resources.open_text(
+            *_shift_path(package, resource),
+            encoding=encoding,
+            errors=errors
+        )
+
+
+    def read_binary(package: Package, resource: Resource) -> bytes:
+        return importlib.resources.read_binary(*_shift_path(package, resource))
+
+
+    def read_text(
+            package: Package,
+            resource: Resource,
+            encoding: str = 'utf-8',
+            errors: str = 'strict',
+    ) -> str:
+        return importlib.resources.read_text(
+            *_shift_path(package, resource),
+            encoding=encoding,
+            errors=errors
+        )
 else:
     def open_binary(package: Package, resource: Resource) -> BinaryIO:
         """Return a file-like object opened for binary reading of the resource."""
