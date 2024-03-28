fc23bedef956387d2e355e317be07c21c87050d6
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2024-03-28 16:57:59 +0100

    Configure logging programmatically.
    logging.config.dictConfig does not work well with custom implementations
    of QueueHandler since Python 3.12. Changed to programmatic configuration.

--- slivka/conf/logging.py
+++ slivka/conf/logging.py
@@ -19,46 +19,31 @@ class ZMQQueueHandler(logging.handlers.QueueHandler):
         socket.connect(address)
         super().__init__(socket)
 
-    def emit(self, record):
-        message = self.format(record)
-        msg = record.__dict__.copy()
-        msg.update(
-            message=message,
-            msg=message,
-            args=None,
-            exc_info=None,
-            exc_text=None
-        )
-        self.queue.send_json(msg)
+    def enqueue(self, record):
+        self.queue.send_json(record.__dict__)
+
+    def close(self):
+        super().close()
+        self.queue.close()
 
 
 class ZMQQueueListener(logging.handlers.QueueListener):
     def __init__(self, address, handlers=(), ctx: zmq.Context = None):
         self._address = address
-        ctx = ctx or _context
-        self._ctx = ctx
-        socket = ctx.socket(zmq.PULL)
+        self._ctx = ctx or _context
+        socket = self._ctx.socket(zmq.PULL)
         socket.bind(address)
         super().__init__(socket, *handlers, respect_handler_level=False)
-        self.handlers = list(self.handlers)
 
     def __enter__(self):
         self.start()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
-        self.cleanup()
-
-    def add_handler(self, handler):
-        self.handlers.append(handler)
-
-    def remove_handler(self, handler):
-        self.handlers.remove(handler)
 
     def dequeue(self, block):
         msg = self.queue.recv_json()
         if msg == self._sentinel:
-            self.queue.close(0)
             return msg
         else:
             return logging.makeLogRecord(msg)
@@ -71,8 +56,6 @@ class ZMQQueueListener(logging.handlers.QueueListener):
     def stop(self):
         super().stop()
         self.queue.close(0)
-
-    def cleanup(self):
         if self._address.startswith('ipc://'):
             os.unlink(self._address[6:])
 
@@ -87,42 +70,25 @@ def get_logging_sock():
     return path
 
 
-def _get_default_logging_config():
-    return {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'full': {
-                'format': "%(asctime)s %(levelname)-10s %(name)s %(message)s",
-                'datefmt': "%d/%m/%y %H:%M:%S"
-            },
-            'minimal': {
-                'format': '%(levelname)s %(message)s'
-            }
-        },
-        'handlers': {
-            'slivka.logging_queue': {
-                'class': 'slivka.conf.logging.ZMQQueueHandler',
-                'formatter': 'full',
-                'level': 'DEBUG',
-                'address': get_logging_sock()
-            },
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'minimal',
-                'level': 'DEBUG'
-            }
-        },
-        'loggers': {
-            'slivka': {
-                'level': 'DEBUG',
-                'propagate': False,
-                'handlers': ['slivka.logging_queue', 'console']
-            }
-        }
-    }
+def configure_logging(level=logging.DEBUG):
+    if isinstance(level, str):
+        level = logging.getLevelName(level)
 
+    zmq_handler = ZMQQueueHandler(get_logging_sock())
+    zmq_handler.setLevel(level)
+    full_formatter = logging.Formatter(
+        fmt="%(asctime)s %(levelname)-8s %(name)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    zmq_handler.setFormatter(full_formatter)
 
-def configure_logging(config=None):
-    config = config or _get_default_logging_config()
-    logging.config.dictConfig(config)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.WARNING)
+    minimal_formatter = logging.Formatter(fmt="%(levelname)-8s %(message)s")
+    console_handler.setFormatter(minimal_formatter)
+
+    logger = logging.getLogger("slivka")
+    logger.propagate = False
+    logger.setLevel(level)
+    logger.addHandler(zmq_handler)
+    logger.addHandler(console_handler)
