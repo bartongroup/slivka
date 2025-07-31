aeeb93df463b6d0069d42822ee1bd70773a1abca
Mateusz Warowny <warowny.mateusz@gene.com>
2025-07-31 15:57:52 +0200

    Create additional unittests for migration_1

--- test/migrations/test_job_directory_migration.py
+++ test/migrations/test_job_directory_migration.py
@@ -1,12 +1,73 @@
+from datetime import datetime
 import os
 import shutil
+from base64 import urlsafe_b64decode
 from importlib import resources
 
 import pytest
 import yaml
 from bson import ObjectId
 
-import slivka.migrations.migration_1
+from slivka.migrations import migration_1
+from slivka.migrations.migration_1 import make_job_path, move_job_directories, normalize_symlinks
+
+
+def test_move_job_directories(database, tmp_path):
+    collection = database['requests']
+    old_dir = tmp_path / 'XXXXXXXXXXXXABCD'
+    old_dir.mkdir()
+    doc = {
+        '_id': ObjectId(urlsafe_b64decode('XXXXXXXXXXXXABCD')),
+        'service': 'example',
+        'inputs': {},
+        'timestamp': datetime(2025, 6, 1, 12, 0),
+        'completion_time': datetime(2025, 6, 1, 12, 1),
+        'status': 6,
+        'runner': 'default',
+        'job': {
+            'work_dir': str(old_dir),
+            'job_id': '0'
+        }
+    }
+    insert_result = collection.insert_one(doc)
+    new_dir = tmp_path / 'CD' / 'AB' / 'XXXXXXXXXXXX'
+    move_result = list(move_job_directories(collection, str(tmp_path)))
+    assert move_result == [(str(old_dir), str(new_dir))]
+    assert not old_dir.is_dir()
+    assert new_dir.is_dir()
+    doc = collection.find_one({"_id" : insert_result.inserted_id})
+    assert doc['job']['work_dir'] == str(new_dir)
+
+
+def test_normalize_symlinks(tmp_path):
+    target = tmp_path / 'target.txt'
+    target.touch()
+    direct_link = tmp_path / 'direct'
+    direct_link.symlink_to(target)
+    indirect_link = tmp_path / 'indirect'
+    indirect_link.symlink_to(direct_link)
+    assert indirect_link.readlink() != target
+    normalize_symlinks(str(tmp_path))
+    assert indirect_link.readlink() == target
+
+
+@pytest.mark.parametrize(
+    ('base_path', 'object_id', 'expected_path'),
+    [
+        (
+            '/home/slivka/',
+            ObjectId(urlsafe_b64decode('XXXXXXXXXXXXABCD')),
+            '/home/slivka/CD/AB/XXXXXXXXXXXX'
+        ),
+        (
+            '/',
+            ObjectId(urlsafe_b64decode('XYZWxyzwXYZWABCD')),
+            '/CD/AB/XYZWxyzwXYZW'
+        )
+    ]
+)
+def test_make_job_path(base_path, object_id, expected_path):
+    assert make_job_path(base_path=base_path, object_id=object_id) == expected_path
 
 
 @pytest.fixture(scope="class")
@@ -14,6 +75,16 @@ def project_files(request, slivka_home):
     traversable = request.param
     with resources.as_file(traversable) as path:
         shutil.copytree(path, slivka_home, dirs_exist_ok=True)
+
+
+@pytest.fixture(scope='class')
+def populate_requests_collection(jobs_top_dir, database):
+    def insert_documents(items):
+        for item in items:
+            item['_id'] = ObjectId(item['_id'])
+            item['job']['work_dir'] = str(jobs_top_dir / item['job']['work_dir'])
+        return list(database['requests'].insert_many(items))
+    return insert_documents
 
 
 @pytest.fixture(scope="class")
@@ -29,7 +100,6 @@ def job_requests(slivka_home, database):
     return database['requests'].insert_many(requests)
 
 
-
 @pytest.mark.parametrize(
     'project_files',
     [
@@ -37,11 +107,10 @@ def job_requests(slivka_home, database):
     ],
     indirect=['project_files']
 )
-@pytest.mark.usefixtures('job_requests')
 class TestApplyMigration:
     @pytest.fixture(scope="class", autouse=True)
-    def run_migration(self, slivka_home, database):
-        slivka.migrations.migration_1.apply(database, str(slivka_home / "jobs"))
+    def run_migration(self, slivka_home, database, project_files, job_requests):
+        migration_1.apply(database, str(slivka_home / "jobs"))
 
     def test_directory_moved(self, slivka_home):
         assert (slivka_home / "jobs" / "AA" / "AA" / "AAAAAAAAAAAA").is_dir()
@@ -56,3 +125,41 @@ class TestApplyMigration:
         assert request_0['job']['work_dir'] == str(slivka_home / 'jobs' / 'AA' / 'AA' / 'AAAAAAAAAAAA')
         request_1 = database['requests'].find_one({'_id': ObjectId('000000000000000000000001')})
         assert request_1['job']['work_dir'] == str(slivka_home / 'jobs' / 'AB' / 'AA' / 'AAAAAAAAAAAA')
+
+
+@pytest.mark.parametrize(
+    'project_files',
+    [
+        resources.files(__package__) / '0.8.5-flat-directory-project',
+    ],
+    indirect=['project_files']
+)
+def test_input_parameters_updated(database, slivka_home, project_files):
+    collection = database['requests']
+    collection.insert_one({
+        '_id': ObjectId(urlsafe_b64decode('AAAAAAAAAAAAAAAA')),
+        'service': 'example',
+        'inputs': {
+            'text': "value"
+        },
+        'job': {
+            'work_dir': str(slivka_home / 'jobs' / 'AAAAAAAAAAAAAAAA'),
+            'job_id': '0'
+        }
+    })
+    insert_result_1 = collection.insert_one({
+        '_id': ObjectId(urlsafe_b64decode('AAAAAAAAAAAAAAAB')),
+        'service': 'example',
+        'inputs': {
+            'text': 'value',
+            'infile': str(slivka_home / 'jobs' / 'AAAAAAAAAAAAAAAA' / 'stdout')
+        },
+        'job': {
+            'work_dir': str(slivka_home / 'jobs' / 'AAAAAAAAAAAAAAAB'),
+            'job_id': '1'
+        }
+    })
+    migration_1.apply(database, str(slivka_home / 'jobs'))
+    request = collection.find_one({'_id': insert_result_1.inserted_id})
+    assert request['inputs']['infile'] == str(slivka_home / 'jobs' / 'AA' / 'AA' / 'AAAAAAAAAAAA' / 'stdout')
+
