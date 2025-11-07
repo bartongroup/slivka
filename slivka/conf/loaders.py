b9ed074d968728d115a67def67a6d9a8ff90942b
Mateusz Warowny <warowny.mateusz@gene.com>
2025-11-07 13:50:25 +0100

    Fix incorrect property name that SLIVKA_DIR_JOBS was mapped to

--- slivka/conf/loaders.py
+++ slivka/conf/loaders.py
@@ -148,7 +148,7 @@ class SettingsLoader_0_8_5b5:
             for var_name, config_prop in [
                 ("SLIVKA_HOME", "directory.home"),
                 ("SLIVKA_DIR_UPLOADS", "directory.uploads"),
-                ("SLIVKA_DIR_JOBS", "directory.uploads"),
+                ("SLIVKA_DIR_JOBS", "directory.jobs"),
                 ("SLIVKA_SERVER_PREFIX", "server.prefix"),
                 ("SLIVKA_SERVER_HOST", "server.host"),
                 ("SLIVKA_LOCAL_QUEUE_HOST", "local-queue.host"),
