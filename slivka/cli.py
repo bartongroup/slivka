9d0f6caffc2a9458592bce9641f01bccdb4e985f
Mateusz Warowny <warowny.mateusz@gene.com>
2025-11-18 14:29:55 +0100

    Add option to exclude example file during project init.

--- slivka/cli.py
+++ slivka/cli.py
@@ -27,7 +27,8 @@ def main():
 
 @main.command('init')
 @click.argument("path", type=click.Path(writable=True))
-def init(path):
+@click.option("--example/--no-example", default=True)
+def init(path, example):
     """Initialize a new project in the directory specified."""
     path = os.path.abspath(os.path.join(os.curdir, path))
     if os.path.isdir(path):
@@ -36,10 +37,10 @@ def init(path):
             "Do you want to overwrite its content?" % path,
             abort=True
         )
-    init_project(path)
+    init_project(path, example)
 
 
-def init_project(base_dir):
+def init_project(base_dir, with_example):
     import shutil
     import stat
     from slivka.compat import resources
@@ -61,11 +62,12 @@ def init_project(base_dir):
     copy_project_file("settings.yaml")
     copy_project_file("wsgi.py")
     copy_project_file("routes.py")
-    copy_project_file("services/example.service.yaml")
-    copy_project_file("scripts/selectors.py")
-    copy_project_file("scripts/example.py")
-    os.chmod(os.path.join(base_dir, 'scripts', 'example.py'), stat.S_IRWXU)
-    copy_project_file("testdata/example-input.txt")
+    if with_example:
+        copy_project_file("services/example.service.yaml")
+        copy_project_file("scripts/selectors.py")
+        copy_project_file("scripts/example.py")
+        os.chmod(os.path.join(base_dir, 'scripts', 'example.py'), stat.S_IRWXU)
+        copy_project_file("testdata/example-input.txt")
     copy_project_file("static/openapi.yaml")
     copy_project_file("static/redoc-index.html")
     copy_project_file("templates/index.html")
